75fdc9efb86251820790b75142f776eaafb71590
silvy <oskar.edu.owk@gmail.com>
2026-03-10 18:29:25 +0000

    Add Logging for U16's

--- image_lib/logging.c
+++ image_lib/logging.c
@@ -6,27 +6,15 @@
 #include <math.h>
 #include <inttypes.h> 
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef int32_t i32;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef float f32;
 
 void log_u32b(u32 number) {
-    printf("The value is: %" PRIu32 "\n", number);
+    printf("%" PRIu32 "\n", number);
 }
 
-typedef struct {
-  u64 real;
-  u64 imag;
-} complex;
+void log_u16b(u16 number) {
+    printf("%" PRIu16 "\n", number);
+}
+
 
 void log_complex(complex number) {
   int byte;
