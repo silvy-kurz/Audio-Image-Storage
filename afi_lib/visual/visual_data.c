9a3bf6a2288945a67b64cfe3827f0fdddea97402
silvy <oskar.edu.owk@gmail.com>
2026-03-11 22:04:33 +0000

    Implement Pixel Types, RGBA 32 Bit Logging, and Pixel Container

--- afi_lib/visual/visual_data.c
+++ afi_lib/visual/visual_data.c
@@ -1,3 +1,8 @@
+typedef enum afi_pixel_type_e {
+  RGBA_32,  
+  RGBA_16F,  
+  RGB_8,  
+} afi_pixel_type_t;
 
 
 typedef struct afi_pixel_u32_s{
@@ -7,3 +12,54 @@ typedef struct afi_pixel_u32_s{
   u32 a;
 } afi_pixel_u32_t;
 
+
+typedef struct afi_pixels_s {
+  afi_pixel_type_t type;
+  int pixel_count;
+  void *pixel_buffer;
+} afi_pixels_t;
+
+
+
+void log_pixel_u32(char *front, afi_pixel_u32_t pixel) {
+  printf("%s", front);
+  log_u32("  R: ", pixel.r);
+  printf("%s", front);
+  log_u32("  G: ", pixel.g);
+  printf("%s", front);
+  log_u32("  B: ", pixel.b);
+  printf("%s", front);
+  log_u32("  A: ", pixel.a);
+}
+
+
+
+
+afi_result_t log_pixels(afi_pixels_t *pixels, int amount) {
+
+  switch (pixels->type) {
+    case RGBA_32 : {
+      printf("Pixels are 32 Bit RGBA\n"); 
+      afi_pixel_u32_t *pixel_buffer = (afi_pixel_u32_t *)pixels->pixel_buffer;
+      
+      if (amount > 0) {
+        printf("Printing First %d Pixels: \n\n", amount);
+        for (int i = 0; i < amount; i++) {
+          printf("  Pixel %d:\n", i);
+          log_pixel_u32("  ", pixel_buffer[i]);
+          printf(" \n");
+        };
+      }
+
+      return AFI_SUCCESS;
+    } 
+    case RGBA_16F : {
+
+      return AFI_TYPE_NOT_SUPPORTED;
+    }
+    case RGB_8 : {
+
+      return AFI_TYPE_NOT_SUPPORTED;
+    }
+  }
+}
