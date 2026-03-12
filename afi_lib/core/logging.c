737080e14d275ea3f55972ec05c7e64228bb4ae0
silvy <oskar.edu.owk@gmail.com>
2026-03-12 19:44:29 +0000

    Add Logging for Null-Terminal Lacking Char Arrays

--- afi_lib/core/logging.c
+++ afi_lib/core/logging.c
@@ -19,6 +19,11 @@ void log_i16(char *front, i16 number) {
     printf("%s%" PRId16 "\n", front, number);
 }
 
-
+void log_char_array(char *front, char *chars, int length) {
+  char printable_char_string[length + 1];
+  memcpy(printable_char_string, chars, sizeof(char) * length);
+  printable_char_string[length] = '\0';
+  printf("%s%s\n", front, printable_char_string);
+}
 
 
